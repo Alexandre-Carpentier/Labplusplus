d65933c1050a88d74beb6b435e302c53db1c4813
Alexandre Carpentier <alex.carpentier@wanadoo.fr>
2024-02-13 08:20:34 +0100

    lua start

--- Plugin/cLua.h
+++ Plugin/cLua.h
@@ -1,5 +1,31 @@
-#pragma once
-class cLua
+#ifndef _LUA_H_
+#define _LUA_H_
+extern "C"
 {
-};
+#include "cLua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+}
+#ifdef _WIN32
+#pragma comment (lib, "lua54.lib")
+#endif
+#endif
+
+// Here define all the binded fonction you can call from LUA
+
+// Device Setter
+int lua_SetDeviceName(lua_State* L);
+int lua_SetDeviceAddr(lua_State* L);
+int lua_SetDeviceProtocol(lua_State* L);
+int lua_SetDeviceUnit(lua_State* L);
+int lua_SetDeviceIdentificationStr(lua_State* L);
+int lua_SetDeviceImg(lua_State* L);
+
+// Plugin builder
+int lua_SetControl(lua_State* L);
+//builder->AddPanelCtrl(CONTROLTYPE::BUT, 150, 20, "Query test equipment?");
+
+
+
+int lua_CreateObject(lua_State* L);
 
